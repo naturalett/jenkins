09e3422d61f21d5431499b6ec7e8ab3b5fb39c8d
lidor ettinger <lidor.ettinger@gmail.com>
2023-01-23 18:23:52 +0200

    Add pipeline jobs

--- Application/hello.py
+++ Application/hello.py
@@ -1,4 +1,5 @@
 import flask
+
 app = flask.Flask(__name__)
 
 @app.route('/home')
---
+++ Jenkins/pipelines/Clone.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent {
+        docker {
+            image 'python'
+            args '--name workshop'
+        }
+    }
+
+  stages {
+    stage('Do job stage') {
+      steps {
+        git branch: 'main', url: 'https://github.com/naturalett/getting-started.git'
+        sh 'ls -al'
+      }
+    }
+  }
+}
